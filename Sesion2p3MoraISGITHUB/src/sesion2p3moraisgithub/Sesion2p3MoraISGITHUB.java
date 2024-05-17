d5074210c21f4a9d2b5482d82d4de9411354b490
155765 <155765@udlondres.com>
2024-05-16 21:51:06 -0600

    Confirmacion MORA 1
    Todo very fine!

---
+++ Sesion2p3MoraISGITHUB/src/sesion2p3moraisgithub/Sesion2p3MoraISGITHUB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion2p3moraisgithub;
+
+/**
+ *
+ * @author jaelm
+ */
+public class Sesion2p3MoraISGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("PRUEBA DE GITHUB MORA");
+    }
+    
+}
